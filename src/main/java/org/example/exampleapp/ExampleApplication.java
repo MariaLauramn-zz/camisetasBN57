9d1ee47661ea03d15b269160ac7eadf88e0ed994
Marialauramn16 <hikco25@gmail.com>
2021-08-26 12:05:11 +0200

    creando archivo system.properties

--- src/main/java/org/example/exampleapp/ExampleApplication.java
+++ src/main/java/org/example/exampleapp/ExampleApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ExampleApplication {
+public class
+ExampleApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ExampleApplication.class, args);
