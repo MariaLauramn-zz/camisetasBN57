dfa12cd488c18dead106ccff97bbc9d857b5f19d
Eliana solsona <elianasolsona@gmail.com>
2021-08-30 15:24:20 +0200

    completando controllers

--- src/main/java/org/camisetas/camisetasapp/controllers/CamisetaController.java
+++ src/main/java/org/camisetas/camisetasapp/controllers/CamisetaController.java
@@ -7,10 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class CamisetaController {
 
     private CamisetaService camisetaService;
@@ -27,4 +30,9 @@ public class CamisetaController {
         model.addAttribute("camisetas", camisetas);
         return "camisetas/all";
     }
+    @PostMapping("/new")
+  String addCamiseta(@ModelAttribute Camiseta camiseta) {
+    camisetaService.save(camiseta);
+    return "redirect:/camisetas";
+  }
 }
