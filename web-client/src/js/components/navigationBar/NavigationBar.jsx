8ba0a94acfbf30b16f0200051fc5625726ee1a33
Marialauramn16 <hikco25@gmail.com>
2021-09-07 13:58:59 +0200

    agregando barra navegación con Bootstrap y scss

--- web-client/src/js/components/navigationBar/NavigationBar.jsx
+++ web-client/src/js/components/navigationBar/NavigationBar.jsx
@@ -1,9 +1,15 @@
 import * as React from "react";
 import {Link} from "react-router-dom";
+import { Nav, Navbar} from "react-bootstrap";
+
 
 export const NavigationBar = () => (
-    <nav className={'navigationBar'}>
-        <Link to={"/shirt"}>Camisetas</Link>
-        <Link to={"/about"}>Sobre nosotros</Link>
-    </nav>
+    <Navbar bg="light" variant="light">
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Nav className="mr-auto nav">
+            <Nav.Link href="#pricing">Inicio</Nav.Link>
+            <Nav.Link href="#home">Camisetas</Nav.Link>
+            <Nav.Link href="#features">Sobre Nosotros</Nav.Link>
+        </Nav>
+    </Navbar>
 )
