2fa53bfab262b33d14ad3f855d58ac1686281e54
Marialauramn16 <hikco25@gmail.com>
2021-09-09 09:55:49 +0200

    actualizando navigationBarç

--- web-client/src/js/components/navigationBar/NavigationBar.jsx
+++ web-client/src/js/components/navigationBar/NavigationBar.jsx
@@ -5,19 +5,11 @@ import { Link } from 'react-router-dom';
 
 export const NavigationBar = () => (
     <Navbar bg="light" variant="light">
-<<<<<<< HEAD
-        <Navbar.Brand as={Link} to="/">Navbar</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">Logo</Navbar.Brand>
         <Nav className="mr-auto nav">
             <Nav.Link as={Link} to="/">Inicio</Nav.Link>
             <Nav.Link as={Link} to="/shirt">Camisetas</Nav.Link>
             <Nav.Link as={Link} to="/about">Sobre Nosotros</Nav.Link>
-=======
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-        <Nav className="mr-auto">
-            <Link to={"/"}>Inicio</Link>
-            <Link to={"/shirt"}>Camisetas</Link>
-            <Link to={"/about"}>Sobre nosotros</Link>
->>>>>>> f7e33d17ee0813a8f5af95d565ade7f9b7330a97
         </Nav>
     </Navbar>
 
