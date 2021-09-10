2f7a690094c25448bbf2f971a90e7705a10c0777
leobarna <leonoradli94@gmail.com>
2021-09-10 13:56:03 +0200

    footer,navbar modificanciones

--- web-client/src/js/components/footer/Footer.jsx
+++ web-client/src/js/components/footer/Footer.jsx
@@ -1,10 +1,18 @@
 import * as React from "react";
-import { footer} from "react-bootstrap";
 import './footer.scss';
-
+import Link from '@material-ui/core/Link';
+import {Instagram, MailOutline} from '@material-ui/icons';
 
 export const Footer = () => (
-    <footer>
-        <p>Hola soy Amanda</p>
+    <footer className="fixed-bottom">
+        <div className="d-flex justify-content-around">
+            <p>Soporte al cliente</p>
+            <div className="social-media">
+                <Link href="https://www.instagram.com/bn57.store/
+" target="_blank"><Instagram/></Link>
+                <Link href="mailto:elcorreoquequieres@correo.com"><MailOutline/></Link>
+            </div>
+        </div>
     </footer>
+
 )
--- web-client/src/js/components/navigationBar/NavigationBar.jsx
+++ web-client/src/js/components/navigationBar/NavigationBar.jsx
@@ -2,11 +2,12 @@ import * as React from "react";
 import {Nav, Navbar} from "react-bootstrap";
 import {Link} from 'react-router-dom';
 import Icon from '@material-ui/icons/AddShoppingCart';
+import './navigationBar.scss';
 
 export const NavigationBar = () => (
-    <Navbar  variant="light" className="d-flex justify-content-around shadow p-3 mb-5 bg-white rounde">
+    <Navbar  variant="light" className="d-flex justify-content-around shadow p-3 mb-5 bg-white ">
         <Navbar.Brand as={Link} to="/" className="">Logo</Navbar.Brand>
-        <Nav className="nav justify-content-center">
+        <Nav className="nav">
             <Nav.Link as={Link} to="/">Inicio</Nav.Link>
             <Nav.Link as={Link} to="/shirt">Camisetas</Nav.Link>
             <Nav.Link as={Link} to="/about">Sobre Nosotros</Nav.Link>
