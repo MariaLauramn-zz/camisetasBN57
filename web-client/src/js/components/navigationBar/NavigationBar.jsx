2d5f5b9247fa115e9bb0bcee1ea33a7ce1f0f347
Marialauramn16 <hikco25@gmail.com>
2021-09-10 13:53:22 +0200

    agregando logo

--- web-client/src/js/components/navigationBar/NavigationBar.jsx
+++ web-client/src/js/components/navigationBar/NavigationBar.jsx
@@ -2,17 +2,19 @@ import * as React from "react";
 import {Nav, Navbar} from "react-bootstrap";
 import {Link} from 'react-router-dom';
 import Icon from '@material-ui/icons/AddShoppingCart';
+import './navigationBar.scss';
+
 
 export const NavigationBar = () => (
-    <Navbar  variant="light" className="d-flex justify-content-around shadow p-3 mb-5 bg-white rounde">
-        <Navbar.Brand as={Link} to="/" className="">Logo</Navbar.Brand>
+    <Navbar variant="light" className="d-flex justify-content-around shadow p-3 mb-5 bg-white rounde">
+        <Navbar.Brand as={Link} to="/"></Navbar.Brand>
         <Nav className="nav justify-content-center">
             <Nav.Link as={Link} to="/">Inicio</Nav.Link>
             <Nav.Link as={Link} to="/shirt">Camisetas</Nav.Link>
             <Nav.Link as={Link} to="/about">Sobre Nosotros</Nav.Link>
             <Nav.Link as={Link} to="/formAddShirt">Formulario</Nav.Link>
         </Nav>
-        <Icon>AddShoppingCartIcon</Icon>
+        <Icon className="carro">AddShoppingCartIcon</Icon>
 
     </Navbar>
 
