4e23a5d4631c052a46c14c7b1bac4e8855cabc77
leobarna <leonoradli94@gmail.com>
2021-09-14 15:12:24 +0200

    update card of catalog

--- web-client/src/js/components/shirt/Shirt.jsx
+++ web-client/src/js/components/shirt/Shirt.jsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
 import {ShirtCard} from "../shirtCard/ShirtCard";
+import {Container} from "react-bootstrap";
 
 export const Shirt = (props) => (
+    <Container>
     <section className={"shirtSection"}>
 
         { props.shirts.map( shirt =>
@@ -10,5 +12,5 @@ export const Shirt = (props) => (
         ) }
 
     </section>
-
+    </Container>
 );
