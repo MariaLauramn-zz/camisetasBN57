29147e05cf0b3a37d35ea17dec9540fafa9c0d6a
leobarna <leonoradli94@gmail.com>
2021-09-14 10:20:16 +0200

    creating cardshirt

--- web-client/src/js/components/shirtCard/ShirtCard.jsx
+++ web-client/src/js/components/shirtCard/ShirtCard.jsx
@@ -1,9 +1,19 @@
 import * as React from 'react';
-
+import "./shirtCard.scss";
+import sampleImage from "../../../imagenes/BN57 siluetada (1).png";
 export const ShirtCard = (props) =>
     <div className="shirt">
-        <div>{ props.shirt.name }</div>
-        <div>{ props.shirt.size}</div>
-        <div>{ props.shirt.color}</div>
-        <div>{ props.shirt.cost}</div>
+        <div className="shirt__img">
+            <img src={sampleImage} alt="sampleImage"/>
+        </div>
+        <div className="shirt__info">
+            <div>{props.shirt.name}</div>
+            <div>{props.shirt.size}</div>
+            <div>{props.shirt.cost}</div>
+        </div>
+        <div className="shirt__buttons">
+            
+        </div>
+
+
     </div>
