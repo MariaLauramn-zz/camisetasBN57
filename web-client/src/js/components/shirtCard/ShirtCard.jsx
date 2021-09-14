378a3cb3d18bc9062a88439fdbd9f3208b5e5bc0
leobarna <leonoradli94@gmail.com>
2021-09-14 11:04:20 +0200

    update shirtCard.jsx

--- web-client/src/js/components/shirtCard/ShirtCard.jsx
+++ web-client/src/js/components/shirtCard/ShirtCard.jsx
@@ -1,18 +1,22 @@
 import * as React from 'react';
 import "./shirtCard.scss";
-import sampleImage from "../../../imagenes/BN57 siluetada (1).png";
+import sampleImage from "../../../imagenes/CABIN CREW.png";
+
 export const ShirtCard = (props) =>
     <div className="shirt">
         <div className="shirt__img">
             <img src={sampleImage} alt="sampleImage"/>
         </div>
         <div className="shirt__info">
-            <div>{props.shirt.name}</div>
-            <div>{props.shirt.size}</div>
-            <div>{props.shirt.cost}</div>
+            <div className="shirt__info__Name">{props.shirt.name}</div>
+            <div className="shirt__info__Categoria">{props.shirt.size}</div>
+            <div className="shirt__infoCost">
+                <div>&euro; {props.shirt.cost}</div>
+            </div>
+
         </div>
         <div className="shirt__buttons">
-            
+
         </div>
 
 
