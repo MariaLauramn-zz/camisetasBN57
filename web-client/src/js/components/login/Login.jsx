17fe3717558ca0e797eb5845911a22c9c1585f1c
leobarna <leonoradli94@gmail.com>
2021-09-20 15:25:46 +0200

    enlazar tabla con btn login

--- web-client/src/js/components/login/Login.jsx
+++ web-client/src/js/components/login/Login.jsx
@@ -13,20 +13,21 @@ export const Login = () => {
                         <Form.Control className="" type="text" placeholder="Enter username"/>
 
                     </Form.Group>
-                        <Form.Group className="mx-4 my-4" controlId="formBasicPassword">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password"/>
-                        </Form.Group>
-                        <Form.Group className="mx-4 d-flex justify-content-between flex-wrap" controlId="formBasicCheckbox">
-                            <Form.Check type="checkbox" label="Remember me"/>
-                            <Link className="form-text small text-muted" href="#">Forgot your password?</Link>
+                    <Form.Group className="mx-4 my-4" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password"/>
                     </Form.Group>
-                        <Form.Group className="mx-4 my-4 pb-4 d-flex justify-content-end
-">
+                    <Form.Group className="mx-4 d-flex justify-content-between flex-wrap" controlId="formBasicCheckbox">
+                        <Form.Check type="checkbox" label="Remember me"/>
+                        <Link className="form-text small text-muted" href="#">Forgot your password?</Link>
+                    </Form.Group>
+                    <Form.Group className="mx-4 my-4 pb-4 d-flex justify-content-end
+"><Link to="/tabla">
                         <Button variant="dark" type="submit">
                             Enviar
                         </Button>
-                        </Form.Group>
+                    </Link>
+                    </Form.Group>
                 </Form>
             </Container>
         </div>
