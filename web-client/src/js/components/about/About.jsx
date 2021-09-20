2ebda2034ccacc124a55e072eaf5a36e26772ed8
Taiza18 <taizabrito@hotmail.com>
2021-09-20 15:11:22 +0200

    cambios en Sobre Nosostros

--- web-client/src/js/components/about/About.jsx
+++ web-client/src/js/components/about/About.jsx
@@ -3,32 +3,30 @@ import "./about.scss";
 import {Container} from "react-bootstrap";
 
 export const About = () =>
+
     <div className="principal">
+
         <Container>
+
             <section className="aboutSection">
                 <div>
-                <h3>HISTORIA BN57</h3>
-                <p>Empezamos con la idea de que todos tienen la possibilidad de poder volar sentirse libre, con
-                    nuestras camisetas te daremos esta sensación de sentirse en las alturas. Con diseños de aviones
-                    divertidos y 100% organico.
-                </p>
+                    <h3>HISTORIA BN57</h3>
+                    <p>Empezamos con la idea de que todos tienen la possibilidad de poder volar sentirse libre, con
+                        nuestras camisetas te daremos esta sensación de sentirse en las alturas. Con diseños de aviones
+                        divertidos y 100% organico.
+                    </p>
                 </div>
                 <div>
                     <h3>NUESTROS VALORES</h3>
-                <p>El equilibrio entre la funcionalidad y la estética.
-                    La simplicidad y la elegancia el minimalismo, porque creemos que menos es más.
-                    Creemos que nos ayuda a crear el mejor producto el hecho de incorporar a nuestros usuarios en los
-                    procesos de diseño.
-                    La experimentación, la innovación y el futuro en cada uno de nuestros productos.
-                </p>
+                    <p>El equilibrio entre la funcionalidad y la estética.
+                        La simplicidad y la elegancia el minimalismo, porque creemos que menos es más.
+                        Creemos que nos ayuda a crear el mejor producto el hecho de incorporar a nuestros usuarios en
+                        los
+                        procesos de diseño.
+                        La experimentación, la innovación y el futuro en cada uno de nuestros productos.
+                    </p>
                 </div>
-                <div>
-                    <h3>NOS GUSTA</h3>
-                <p> Nbn57 es simplicidad es futuro, creatividad, elegancia y productos bien elaborados, es sentirse
-                    libre vida en las alturas inteligencia artificial, seriados de Netflix y HBO, es inovador,
-                    industrial, diferente es possibilidad, conquista de paises es ciencia siempre hacia delante.
-                </p>
-                </div>
+
             </section>
         </Container>
     </div>
